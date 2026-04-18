a279aa34c014f9b8279fa9451594fe97cd12efa9
DylanO9 <dylan.olivares11@gmail.com>
2026-04-18 07:05:26 +0000

    Add login and signup as separate routing pages

---
+++ src/frontend/app/login/page.tsx
@@ -0,0 +1,26 @@
+export default function Login() {
+  return (
+    <main className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans p-8">
+        <h2 className="mb-3">Login</h2>
+        <form id="signup" className="block rounded-sm border p-8 justify-items-center">
+          <div>
+            <label className="block" htmlFor="first_name">First Name:</label>
+            <input className="block border" type="text" id="first_name" name="first_name"/>
+          </div>
+          <div>
+            <label className="block" htmlFor="last_name">Last Name:</label>
+            <input className="block border" type="text" id="last_name" name="last_name"/>
+          </div>
+          <div>
+            <label className="block" htmlFor="email">Email:</label>
+            <input className="block border"type="text" id="email" name="email"/>
+          </div>
+          <div>
+            <label className="block" htmlFor="password">Password:</label>
+            <input className="block border" type="text" id="password" name="password"/>
+          </div>
+          <button className="border mt-4 p-1 cursor-pointer">Login</button>
+        </form>
+    </main>
+  );
+}
