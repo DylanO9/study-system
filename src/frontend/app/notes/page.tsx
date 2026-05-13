84e794b152df817bee973e9ea11d0e9c65dac008
DylanO9 <dylan.olivares11@gmail.com>
2026-05-13 18:05:08 +0000

    Add a generate insights button that does nothing right now

--- src/frontend/app/notes/page.tsx
+++ src/frontend/app/notes/page.tsx
@@ -6,12 +6,20 @@ export default function Notes() {
             <Navbar/>
             {/* Main Body */}
             <div id='main-dashboard' className='flex-1 p-8'>
-                <label className="block">Input Notes in .md Format</label>
-                <textarea
-                    className="border w-full h-full p-8"
-                    rows={33}
-                    maxLength={5000}
-                />
+                <div className="border-t border-l border-r w-full h-1/2 flex flex-col p-8 items-start">
+                    <h2 className="mb-4">Input Notes</h2>
+                    <textarea
+                        className="border w-full resize-none mb-4"
+                        rows={33}
+                        maxLength={5000}
+                    />
+                    <button id="generate-insights" className="cursor-pointer self-center border p-2">Generate Insights</button>
+                </div>
+        
+                <div className="border w-full h-1/2 p-8 flex flex-col">
+                    <h2 className="mb-4">Insights</h2>
+                    <p id="response" className="border flex w-full h-full"></p>
+                </div>
             </div>
         </main>
     );
